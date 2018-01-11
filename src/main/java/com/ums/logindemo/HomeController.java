5c8ad09b5be27340a3961251788434eee1f6cf48
liujianf8 <liujianf8@sina.com>
2018-01-11 10:25:27 +0800

    first commit

--- src/main/java/com/ums/logindemo/HomeController.java
+++ src/main/java/com/ums/logindemo/HomeController.java
@@ -17,28 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 *拦截所有对根的访问返回到登录页面
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		return "login";
 	}
 	
-	/*
-	 * 拦截对login路由的访问
-	 * */
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String login(String username,String password,Model model) {
-		        //验证传递过来的参数是否正确，否则返回到登陆页面。
 				if(username.equals("gameloft9")&&password.equals("123")){
 					model.addAttribute("username", username);
 					model.addAttribute("password", password);
 					return "result";
 				}
-				return "login";//返回登录页面
+				return "login";
 	}
 	
 }
